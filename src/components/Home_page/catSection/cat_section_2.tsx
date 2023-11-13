9191bf8f43333d3c23b236b37a62a871b29ad729
arcadio <c4net.7@gmail.com>
2023-11-13 15:44:16 +0330

    improve homepage catSection style for mobile view

--- src/components/Home_page/catSection/cat_section_2.tsx
+++ src/components/Home_page/catSection/cat_section_2.tsx
@@ -11,38 +11,43 @@ interface Props {
 const Cat_section_2 = async ({ cats }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center gap-7 py-6">
-      <h1 className="mb-5 text-xl bg-slate-50 w-full py-4 bg-opacity-40 text-center font-iranyekan_bold">
+      <h1 className="mb-5 text-2xl bg-slate-50 w-full py-4 bg-opacity-40 text-center font-iranyekan_bold">
         خرید بر اساس دسته بندی
       </h1>
-      {cats.map((mainCat) => {
-        return (
-          <div className="flex gap-2" key={mainCat.id}>
-            {mainCat.Specific_cat.map((specific) => {
-              return (
-                <div
-                  className="relative p-2 transition-all duration-150 hover:scale-[1.015] hover:shadow-lg  flex items-center  bg-opacity-40 rounded-lg  flex-col justify-between gap-4"
-                  key={specific.id}
-                >
-                  <Link
-                    className="flex flex-col gap-2 items-center justify-between h-full"
-                    href={`/search/${specific.label}`}
+      <div className="w-full px-2 flex flex-col-reverse items-center justify-center gap-4">
+        {cats.map((mainCat) => {
+          return (
+            <div
+              className="flex gap-2 justify-center flex-wrap items-stretch"
+              key={mainCat.id}
+            >
+              {mainCat.Specific_cat.map((specific) => {
+                return (
+                  <div
+                    className="border-4 rounded-lg border-dark_6 border-opacity-10 hover:border-transparent p-2 transition-all duration-150 hover:scale-[1.015] hover:shadow-lg flex items-center grow flex-col justify-between gap-4"
+                    key={specific.id}
                   >
-                    <Image
-                      src={specific.single_image}
-                      width={100}
-                      height={100}
-                      alt={specific.title}
-                    />
-                    <h2 className="font-iranyekan text-dark_2 text-xl">
-                      {specific.title}
-                    </h2>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                    <Link
+                      className="flex flex-col gap-2 items-center justify-between h-full"
+                      href={`/search/${specific.label}`}
+                    >
+                      <Image
+                        src={specific.single_image}
+                        width={100}
+                        height={100}
+                        alt={specific.title}
+                      />
+                      <h2 className="font-iranyekan_bold text-dark_2 text-xl">
+                        {specific.title}
+                      </h2>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
