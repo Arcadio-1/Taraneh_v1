f14659bc648bb7b930e6d2d298e8915b888ba804
arcadio <c4net.7@gmail.com>
2023-11-28 17:11:10 +0330

    add side menu for cart menu

--- src/components/Util/nav/components/shoping_cart/ShoppingCartButton.tsx
+++ src/components/Util/nav/components/shoping_cart/ShoppingCartButton.tsx
@@ -8,40 +8,83 @@ import {
 } from "@/components_shadcn/ui/hover-card";
 import Shopping_cart from "./components/shopping_cart";
 import Link from "next/link";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components_shadcn/ui/popover";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components_shadcn/ui/sheet";
 interface Props {
   cart: ShoppingCart | null;
 }
 
 const ShoppingCartButton = ({ cart }: Props) => {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <>
-      {cart && cart.size > 0 ? (
-        <div>
-          <HoverCard openDelay={0} closeDelay={0}>
-            <HoverCardTrigger asChild className="">
-              <div className="relative p-1 mx-2 duration-0 data-[state=open]:bg-red-100 rounded-lg">
-                <span className="absolute bottom-[-5px] right-[-7px] bg-g1_5 bg-opacity-90 text-light_2 py-[1px] px-2 flex items-center justify-center rounded-[5px] font-iransansnum text-xl">
-                  {cart.size}
-                </span>
-                <CartIcon classes="h-9 w-9 fill-dark_4" />
-              </div>
-            </HoverCardTrigger>
-            <HoverCardContent className="w-[40rem] rounded-[5px] bg-white z-50">
-              <Shopping_cart cart={cart} />
-            </HoverCardContent>
-          </HoverCard>
-        </div>
-      ) : (
-        <Link
-          href={"/chekout"}
-          className="relative p-1 mx-2 duration-0 data-[state=open]:bg-red-100 rounded-lg"
-        >
-          <span className="absolute bottom-[0px] right-[-7px] bg-g1_5 bg-opacity-90 text-light_2 py-[1px] px-2 flex items-center justify-center rounded-[5px] font-iransansnum">
-            0
-          </span>
-          <CartIcon classes="h-9 w-9 fill-dark_4" />
-        </Link>
-      )}
+      <div className="hidden md:flex">
+        {cart && cart.size > 0 ? (
+          <div>
+            <HoverCard openDelay={0} closeDelay={0}>
+              <HoverCardTrigger asChild className="">
+                <div className="relative p-1 mx-2 duration-0 data-[state=open]:bg-red-100 rounded-lg">
+                  <span className="absolute bottom-[-5px] right-[-7px] bg-g1_5 bg-opacity-90 text-light_2 py-[1px] px-2 flex items-center justify-center rounded-[5px] font-iransansnum text-xl">
+                    {cart.size}
+                  </span>
+                  <CartIcon classes="h-9 w-9 fill-dark_4" />
+                </div>
+              </HoverCardTrigger>
+              <HoverCardContent className="w-[40rem] rounded-[5px] bg-white z-50">
+                <Shopping_cart cart={cart} />
+              </HoverCardContent>
+            </HoverCard>
+          </div>
+        ) : (
+          <Link
+            href={"/chekout"}
+            className="relative p-1 mx-2 duration-0 data-[state=open]:bg-red-100 rounded-lg"
+          >
+            <span className="absolute bottom-[0px] right-[-7px] bg-g1_5 bg-opacity-90 text-light_2 py-[1px] px-2 flex items-center justify-center rounded-[5px] font-iransansnum">
+              0
+            </span>
+            <CartIcon classes="h-9 w-9 fill-dark_4" />
+          </Link>
+        )}
+      </div>
+      <div className="flex md:hidden">
+        {cart && cart.size > 0 ? (
+          <div>
+            <Sheet>
+              <SheetTrigger asChild>
+                <div className="relative p-1 mx-2 duration-0 data-[state=open]:bg-red-100 rounded-lg cursor-pointer">
+                  <span className="absolute bottom-[-5px] right-[-7px] bg-g1_5 bg-opacity-90 text-light_2 py-[1px] px-2 flex items-center justify-center rounded-[5px] font-iransansnum text-xl">
+                    {cart.size}
+                  </span>
+                  <CartIcon classes="h-9 w-9 fill-dark_4" />
+                </div>
+              </SheetTrigger>
+              <SheetContent side={"left"}>
+                <Shopping_cart cart={cart} />
+              </SheetContent>
+            </Sheet>
+          </div>
+        ) : (
+          <Link
+            href={"/chekout"}
+            className="relative p-1 mx-2 duration-0 data-[state=open]:bg-red-100 rounded-lg"
+          >
+            <span className="absolute bottom-[0px] right-[-7px] bg-g1_5 bg-opacity-90 text-light_2 py-[1px] px-2 flex items-center justify-center rounded-[5px] font-iransansnum">
+              0
+            </span>
+            <CartIcon classes="h-9 w-9 fill-dark_4" />
+          </Link>
+        )}
+      </div>
     </>
   );
 };
